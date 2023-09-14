c462035f2e50f32a7ff53bd107e24563f79946b4
David Alejandro Gómez <david@acapillatech.com.ar>
2023-09-14 09:05:31 -0300

    Actualizacion de slideshow para incorporar sorteos

--- front-page.php
+++ front-page.php
@@ -11,6 +11,12 @@ get_header();
     <div id="hero-carousel" class="owl-carousel">
       <div class="item">
         <picture>
+          <source srcset="<?php echo get_stylesheet_directory_uri(); ?>/img/front-page/sorteo-sm.jpg" media="(max-width: 576px)" />
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/front-page/sorteo-lg.jpg" alt="Sorteo Día de la Odontología Latinoamericana - Sorteo 3 de Octubre">
+        </picture>
+      </div>
+      <div class="item">
+        <picture>
           <source srcset="<?php echo get_stylesheet_directory_uri(); ?>/img/front-page/cover-sm.jpg" media="(max-width: 576px)" />
           <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/front-page/cover-lg.jpg" alt="Bienvenidos a Federación Odontológica de la Provincia de Córdoba - Nos une trabajar para revalorizar nuestra profesión">
         </picture>
