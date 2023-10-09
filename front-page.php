f6a9405fad9ce05cd273b6a24cfec0257c438c6f
David Alejandro Gómez <david@acapillatech.com.ar>
2023-10-09 09:02:41 -0300

    Retiro de slideshow sorteos en front-page

--- front-page.php
+++ front-page.php
@@ -10,14 +10,6 @@ get_header();
   <div id="hero-wrapper" class="width-100">
     <div id="hero-carousel" class="owl-carousel">
       <div class="item">
-        <a href="https://fopc.org.ar/noticias/sorteo-dia-de-la-odontologia-latinoamericana/">
-          <picture>
-            <source srcset="<?php echo get_stylesheet_directory_uri(); ?>/img/front-page/sorteo-sm.jpg" media="(max-width: 576px)" />
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/front-page/sorteo-lg.jpg" alt="Sorteo Día de la Odontología Latinoamericana - Sorteo 3 de Octubre">
-          </picture>
-        </a>
-      </div>
-      <div class="item">
         <picture>
           <source srcset="<?php echo get_stylesheet_directory_uri(); ?>/img/front-page/cover-sm.jpg" media="(max-width: 576px)" />
           <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/front-page/cover-lg.jpg" alt="Bienvenidos a Federación Odontológica de la Provincia de Córdoba - Nos une trabajar para revalorizar nuestra profesión">
