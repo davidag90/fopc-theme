80794a9acb1aabcdd9d011bd37c2afb811cc57a7
David Alejandro Gómez <david@acapillatech.com.ar>
2024-01-02 02:06:38 -0300

    Eliminamos coloreado alternativo OS pagas/impagas

--- js/ultimo-mes.js
+++ js/ultimo-mes.js
@@ -1,19 +1,5 @@
 jQuery(function($) {
   $(document).ready(function () {
-    // Colorear filas según si la OS pagó o no
-    $('#tablepress-5 > tbody > tr').each(function() {
-      var textoFila = $(this).children('td:nth-of-type(4)').text().toLowerCase();
-      console.log(textoFila);
-
-      if(textoFila.indexOf("si") !== -1) {
-        $(this).children().addClass('table-success');
-      }
-
-      if(textoFila.indexOf("no") !== -1) {
-        $(this).children().addClass('table-danger');
-      }
-    });
-
     // Capturar el evento 'input' del input de texto
     $("#terminos-filtro").on("input", function () {
       var textoFiltro = $(this).val().toLowerCase(); // Obtener el valor del input y convertirlo a minúsculas
