cf7ef1c0d2bf2225a3983c1a6d21d98828e24847
davidag90 <143510252+davidag90@users.noreply.github.com>
2024-02-26 11:28:15 -0300

    Autoload de tabs basado en hash url

--- js/tabs.js
+++ js/tabs.js
@@ -1,15 +1,39 @@
-jQuery(function($) {
-    var triggerTabList = [].slice.call(document.querySelectorAll('#fopc-tabs a'))
-    
-    triggerTabList.forEach(function (triggerEl) {
-    
-        var tabTrigger = new bootstrap.Tab(triggerEl)
+// Get the current URL
+const currentURL = window.location.href;
+const tabList = [].slice.call(document.querySelectorAll('#fopc-tabs a'));
 
+document.addEventListener('DOMContentLoaded', createTabs(tabList));
+
+// Check if the URL contains a hashtag
+if(currentURL.includes("#")) {
+    const urlHash = window.location.hash;
+
+    document.addEventListener('DOMContentLoaded', fireInitTab(urlHash, tabList));
+}
+
+function fireInitTab(urlHash, tabList) {
+    tabList.forEach(triggerEl => {
+        triggerEl.classList.remove('active');
+        triggerEl.classList.remove('show');
+    });
+
+    let triggerLink = triggerEl.getAttribute('href');
+
+    if(triggerLink === urlHash) {
+        triggerEl.classList.add('active');
+        triggerEl.classList.add('show');
+    }
+}
+
+function createTabs(tabList) {
+    tabList.forEach(triggerEl => {
+        let tabTrigger = new bootstrap.Tab(triggerEl);
+    
         triggerEl.addEventListener('click', function (event) {
-            event.preventDefault()
-            tabTrigger.show()
-        })
-    })
-
-    $('.tab-pane').addClass('fade');
-});
+            event.preventDefault();
+            tabTrigger.show();
+        });
+        
+        triggerEl.classList.add('fade');
+    });
+}
