786706154cc3a406d2bce87768dbf23e0d77ab81
David Alejandro Gómez <david@acapillatech.com.ar>
2024-06-22 14:57:42 -0300

    Rework de manejo de adjuntos en pedidos de tienda

--- functions.php
+++ functions.php
@@ -1013,7 +1013,7 @@ function carousel_circulos_odontologicos_init() {
 add_shortcode('carousel-circulos-odontologicos', 'carousel_circulos_odontologicos_init');
 
 // ------------
-// Shortcode para presentacion de custom-post-types
+// Shortcode para presentacion de Clasificados
 // ------------
 function loop_fed_clasificados() {
   ob_start();
@@ -1340,80 +1340,125 @@ add_filter( 'woocommerce_payment_gateways', 'agregar_pago_deb_liq_prof' );
 }
 
 // Campo personalizado de subida de archivos en Woocommerce
- add_action( 'woocommerce_after_order_notes', 'custom_checkout_file_upload' );
- 
- function custom_checkout_file_upload() {
-    echo '<h3>Personalización</h3>';
-    echo '<p>Adjunte aquí un archivo/foto de su diseño o logotipo propio</p>';
-    echo '<p class="form-row form-row-wide"><label for="img_personalizada">Seleccionar archivo</label><span class="woocommerce-input-wrapper"><input type="file" id="img_personalizada" name="img_personalizada" accept=".jpg,.jpeg,.png,.pdf"><input type="hidden" name="img_personalizada_field"></span></p>';
-    
-    wc_enqueue_js(
-      "$('#img_personalizada').change( function() {
+add_action('woocommerce_after_order_notes', 'custom_checkout_file_upload');
+
+function custom_checkout_file_upload()
+{
+  echo '<h3>Personalización</h3>';
+  echo '<p>Adjunte aquí archivos/fotos de sus diseños o logotipos propios</p>';
+  echo '<p class="form-row form-row-wide"><label for="img_personalizada">Seleccionar archivos</label><span class="woocommerce-input-wrapper"><input type="file" id="img_personalizada" name="img_personalizada[]" accept=".jpg,.jpeg,.png,.pdf" multiple><input type="hidden" name="img_personalizada_field"></span></p>';
+
+  wc_enqueue_js(
+    "$('#img_personalizada').change( function() {
         if ( this.files.length ) {
-            const file = this.files[0];
-            const formData = new FormData();
-            formData.append( 'img_personalizada', file );
-            $.ajax({
-              url: wc_checkout_params.ajax_url + '?action=imgupload',
-              type: 'POST',
-              data: formData,
-              contentType: false,
-              enctype: 'multipart/form-data',
-              processData: false,
-              success: function ( response ) {
-                $( 'input[name=\"img_personalizada_field\"]' ).val( response );
-              },
-              error: function(xhr, status, error) {
-                // Manejo de errores si la carga falla
-                console.log('Error al subir el archivo: ' + error);
-              }
-            });
+          const formData = new FormData();
+          $.each(this.files, function(i, file) {
+            formData.append('img_personalizada[]', file);
+          });
+          $.ajax({
+            url: wc_checkout_params.ajax_url + '?action=imgupload',
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            enctype: 'multipart/form-data',
+            processData: false,
+            success: function ( response ) {
+              $( 'input[name=\"img_personalizada_field\"]' ).val( response );
+            },
+            error: function(xhr, status, error) {
+              console.log('Error al subir los archivos: ' + error);
+            }
+          });
         }
       });"
-    );
- }
-  
- add_action( 'wp_ajax_imgupload', 'custom_imgupload' );
- add_action( 'wp_ajax_nopriv_imgupload', 'custom_imgupload' );
-  
- function custom_imgupload() {
-    global $wpdb;
-    $uploads_dir = wp_upload_dir();
-    if ( isset( $_FILES['img_personalizada'] ) ) {
-       if ( $upload = wp_upload_bits( $_FILES['img_personalizada']['name'], null, file_get_contents( $_FILES['img_personalizada']['tmp_name'] ) ) ) {
-          echo $upload['url'];
-       }
-    }
-    die;
- }
- 
- add_action( 'woocommerce_checkout_update_order_meta', 'custom_save_new_checkout_field' );
-    
- function custom_save_new_checkout_field( $order_id ) { 
-    if ( ! empty( $_POST['img_personalizada_field'] ) ) {
-        update_post_meta( $order_id, '_img_personalizada', $_POST['img_personalizada_field'] );
-    }
- }
-    
- add_action( 'woocommerce_admin_order_data_after_billing_address', 'custom_show_new_checkout_field_order', 10, 1 );
-     
- function custom_show_new_checkout_field_order( $order ) {    
-  $order_id = $order->get_id();
-  
-  $img_personalizada_url = get_post_meta( $order_id, '_img_personalizada', true );
+  );
+}
 
-  if ( $img_personalizada_url ) {
-      echo '<p><strong>Imagen personalizada:</strong> <a href="' . esc_url( $img_personalizada_url ) . '" target="_blank">' . esc_html( $img_personalizada_url ) . '</a></p>';
+add_action( 'wp_ajax_imgupload', 'custom_img_upload' );
+add_action( 'wp_ajax_nopriv_imgupload', 'custom_img_upload' );
+
+function custom_img_upload() {
+  $upload_dir = wp_upload_dir();
+  $files = $_FILES['img_personalizada'];
+  $uploaded_files = array();
+
+  foreach ($files['name'] as $key => $value) {
+    if ($files['error'][$key] === 0) {
+      $file_name = sanitize_file_name($files['name'][$key]);
+      $file_temp = $files['tmp_name'][$key];
+      $file_dest = $upload_dir['path'] . '/' . $file_name;
+
+      if (move_uploaded_file($file_temp, $file_dest)) {
+        $uploaded_files[] = $upload_dir['url'] . '/' . $file_name;
+      }
+    }
   }
- }
-   
- add_action( 'woocommerce_email_after_order_table', 'custom_show_new_checkout_field_emails', 20, 4 );
-    
- function custom_show_new_checkout_field_emails( $order, $sent_to_admin ) {
-     if ( $sent_to_admin && get_post_meta( $order->get_id(), '_img_personalizada', true ) ) echo '<p><strong>Imagen Personalizada:</strong> ' . get_post_meta( $order->get_id(), '_img_personalizada', true ) . '</p>';
- }
 
- // Filtro para añadir una clase a los elementos <a> del widget "Categorías de producto"
+  echo json_encode($uploaded_files);
+  wp_die();
+}
+
+add_action('woocommerce_checkout_update_order_meta', 'save_custom_img_field');
+
+function save_custom_img_field($order_id)
+{
+  if (!$order_id) {
+    return;
+  }
+
+  $order = wc_get_order($order_id);
+
+  if (!empty($_POST['img_personalizada_field'])) {
+    $files = json_decode(stripslashes($_POST['img_personalizada_field']), true);
+    
+    if (is_array($files)) {
+      update_post_meta($order_id, '_img_personalizada_files', $files);
+    }
+  }
+}
+
+add_action('woocommerce_admin_order_data_after_billing_address', 'show_custom_img_field', 10, 1);
+
+function show_custom_img_field($order)
+{
+  $files = get_post_meta($order->get_id(), '_img_personalizada_files', true);
+  if (!empty($files)) {
+      echo '<h3>Archivos personalizados</h3>';
+      echo '<ul>';
+      foreach ($files as $file) {
+          echo '<li><a href="' . esc_url($file) . '" target="_blank">' . basename($file) . '</a></li>';
+      }
+      echo '</ul>';
+  }
+}
+
+add_filter('woocommerce_email_attachments', 'attach_custom_img_files_admin_email', 10, 3);
+
+function attach_custom_img_files_admin_email($attachments, $email_id, $order)
+{
+  if ($email_id === 'new_order') {
+    // Obtén el ID de la orden
+    $order_id = $order->get_id();
+
+    // Obtén los archivos de la metadata de la orden
+    $files = get_post_meta($order_id, '_img_personalizada_files', true);
+
+    if (is_array($files) && !empty($files)) {
+      foreach ($files as $file_url) {
+        $file_path = str_replace(wp_upload_dir()['baseurl'], wp_upload_dir()['basedir'], $file_url);
+
+        if (file_exists($file_path)) {
+          $attachments[] = $file_path;
+        }
+      }
+    }
+  }
+
+  return $attachments;
+}
+
+
+// Filtro para añadir una clase a los elementos <a> del widget "Categorías de producto"
 function custom_product_cat_class($output, $args) {
   if (is_active_widget(false, false, 'woocommerce_product_categories', true) && isset($args['taxonomy']) && $args['taxonomy'] === 'product_cat') {
     $output = str_replace('<a', '<a class="link-light"', $output);
