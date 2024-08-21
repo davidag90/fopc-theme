5ed1d67cbb17fcc9bdde00113596a1571ed2b8a7
David Gomez <info@dagdev.com.ar>
2024-08-21 12:12:15 -0300

    Fix: default to original 404 page from Bootscore

--- 404.php
+++
@@ -1,8 +0,0 @@
-<?php
-// URL a la que deseas redireccionar al visitante
-$redireccionURL = "https://fopc.org.ar/";
-
-// Redireccionar al visitante
-header("Location: " . $redireccionURL);
-exit; // Asegurarse de que el script se detenga después de la redirección
-?>
